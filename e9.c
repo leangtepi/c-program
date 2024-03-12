1c323ca5497111b90d25a72f084959a248e1d8e9
leangtepi <tepileang3@gmail.com>
2024-03-12 12:13:01 +0700

    c program

--- e9.c
+++ e9.c
@@ -1,5 +1,29 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello world:");
+    int ID, Age;
+    char Name[20];
+    char Gender;
+    printf("Enter your ID: ");
+    scanf("%i", &ID);
+    printf("Enter your Name: ");
+    scanf("%s", Name);
+    printf("Enter your Gender: ");
+    scanf(" %c", &Gender);
+    printf("Enter your Age: ");
+    scanf(" %i", &Age);
+    if(Age >= 18)
+    {
+        printf("ID :%d",ID);
+        printf("Name :%s",Name);
+        printf("Gender: %c",Gender);
+        printf("You are enligiable to vote");
+    }
+    else
+    {
+        printf("ID :%d",ID);
+        printf("Name :%s",Name);
+        printf("Gender: %c",Gender);
+        printf("You are not enligiable to vote");
+    }
 }
